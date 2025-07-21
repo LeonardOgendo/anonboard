50a16bd1331d7f27a0cf19dad04e854357125cbe
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-07-21 13:15:03 +0300

    Refactor project | Solidify project structure

---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import registerRoutes from "./routes.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+registerRoutes(app);
+
+export default app;
